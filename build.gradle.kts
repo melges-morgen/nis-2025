4acad6202211bb61cdc91c5eb042ca694f2e9473
Matvey Morgen <melges.morgen@gmail.com>
2025-09-22 18:57:51 +0300

    Add version gen

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,10 @@ plugins {
     kotlin("jvm") version "2.1.21"
 }
 
-group = "ru.uranteh.acauntrix"
-version = "1.0-SNAPSHOT"
+val buildNumber = project.findProperty("BUILD_NUMBER") ?: "SNAPSHOT"
+
+group = "ru.hse.pipo"
+version = "0.0-${buildNumber}"
 
 repositories {
     mavenCentral()
